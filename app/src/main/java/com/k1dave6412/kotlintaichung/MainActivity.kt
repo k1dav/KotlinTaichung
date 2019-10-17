e980ed1b878967aedf68c8efa983015f35ccd328
PC <k1dave6412@gmail.com>
2019-10-17 11:37:53 +0800

    feat(init): init

---
+++ app/src/main/java/com/k1dave6412/kotlintaichung/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.k1dave6412.kotlintaichung
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
